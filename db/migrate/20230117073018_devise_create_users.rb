a86e2f286e9612375d0addc4f4d84aa011c748f9
arakikeita <araki1728@gmail.com>
2023-01-17 16:40:42 +0900

    usersテーブル作成

--- db/migrate/20230117073018_devise_create_users.rb
+++ db/migrate/20230117073018_devise_create_users.rb
@@ -6,6 +6,12 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :nickname, null: false
+      t.string :surname, null: false
+      t.string :name, null: false
+      t.string :surnamek, null: false
+      t.string :namek, null: false 
+      t.date :birthday,null: false
 
       ## Recoverable
       t.string   :reset_password_token
