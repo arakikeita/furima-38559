5c498afa701f352c4b514332c12dd1ca9b9e8f7d
arakikeita <araki1728@gmail.com>
2023-01-17 21:11:21 +0900

    バリデーション入力

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,23 @@ class User < ApplicationRecord
   has_many :items
   has_many :buys
 
+  validates :nickname, presence: true
+  validates :email, presence: true
+  validates :encrypted_password, presence: true
+  
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+    validates :surname
+    validates :name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶ一ー]+\z/, message: '全角カナを使用してください' } do
+    validates :surnamek
+    validates :namek
+  end
+
+  validates :birthday, presence: true
+
+
+
+
 end
