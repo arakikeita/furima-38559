70e55321744785c240266d581a38b268737bdf9b
arakikeita <araki1728@gmail.com>
2023-01-23 20:09:17 +0900

    JSコード入力完了

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :item do
+    name {Faker::Commerce.name}
+    context {Faker::Lorem.paragraph}
     
+    category_id {Faker::Number.between(from: 0, to: 9)}
+    state_id {Faker::Number.between(from: 0, to: 6)}
+    shipping_id {Faker::Number.between(from: 0, to: 2)}
+    area_id {Faker::Number.between(from: 0, to: 48)}
+    scheduled_id {Faker::Number.between(from: 0, to: 3)}
+
+    price {Faker::Number.between(from: 300, to: 9999999)}
   end
 end
