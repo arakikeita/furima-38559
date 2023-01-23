35f6fc1550dfb4b568525eac6b22e72db0da8372
arakikeita <araki1728@gmail.com>
2023-01-24 08:07:53 +0900

    itemモデルテストコード実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,14 +1,20 @@
 FactoryBot.define do
   factory :item do
-    name {Faker::Commerce.name}
-    context {Faker::Lorem.paragraph}
+    association :user
+
+    name {Faker::Name.name}
+    content {Faker::Lorem.paragraph}
     
-    category_id {Faker::Number.between(from: 0, to: 9)}
-    state_id {Faker::Number.between(from: 0, to: 6)}
-    shipping_id {Faker::Number.between(from: 0, to: 2)}
-    area_id {Faker::Number.between(from: 0, to: 48)}
-    scheduled_id {Faker::Number.between(from: 0, to: 3)}
+    category_id {Faker::Number.between(from: 1, to: 9)}
+    state_id {Faker::Number.between(from: 1, to: 6)}
+    shipping_id {Faker::Number.between(from: 1, to: 2)}
+    area_id {Faker::Number.between(from: 1, to: 48)}
+    scheduled_day_id {Faker::Number.between(from: 1, to: 3)}
 
     price {Faker::Number.between(from: 300, to: 9999999)}
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,6 +2,68 @@ require 'rails_helper'
 
 RSpec.describe Item, type: :model do
   before do
-    @user = FactoryBot.build(:user)
-end
+    @item = FactoryBot.build(:item)
+  end
 
+  describe '商品出品機能' do
+      context '出品できるとき' do
+        it '全て入力されていると登録ができる' do 
+          expect(@item).to be_valid
+        end
+      end
+
+      context '出品できないとき' do
+        it 'imageが空だと出品できない' do 
+          @item.image = nil
+          @item.valid?
+          expect(@item.errors.full_messages).to include("Image can't be blank")          
+        end
+        it 'nameが空だと出品できない' do
+          @item.name = ''
+          @item.valid?
+          expect(@item.errors.full_messages).to include("Name can't be blank")
+        end
+        it 'contentが空だと出品できない' do
+          @item.content = ''
+          @item.valid?
+          expect(@item.errors.full_messages).to include("Content can't be blank")
+        end
+        it 'category_idが空だと出品できない' do
+          @item.category_id = ''
+          @item.valid?
+          expect(@item.errors.full_messages).to include("Category is not a number")
+        end
+        it 'state_idが空だと出品できない' do
+          @item.state_id = ''
+          @item.valid?
+          expect(@item.errors.full_messages).to include("State is not a number")
+        end
+        it 'shipping_idが空だと出品できない' do
+          @item.shipping_id = ''
+          @item.valid?
+          expect(@item.errors.full_messages).to include("Shipping is not a number")
+        end
+        it 'area_idが空だと出品できない' do
+          @item.area_id = ''
+          @item.valid?
+          expect(@item.errors.full_messages).to include("Area is not a number")
+        end
+        it 'scheduled_day_idが空だと出品できない' do
+          @item.scheduled_day_id = ''
+          @item.valid?
+          expect(@item.errors.full_messages).to include("Scheduled day is not a number")
+        end
+        it 'priceが空だと出品できない' do
+          @item.price = ''
+          @item.valid?
+          expect(@item.errors.full_messages).to include("Price can't be blank")
+        end
+        it 'priceが300~9999999に当てはまらないと出品できない' do
+          @item.price = '1'
+          @item.valid?
+          expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+          
+        end
+      end
+  end
+end
