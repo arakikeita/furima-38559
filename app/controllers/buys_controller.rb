860326acba223f057409c51b6da83f9d7b7bfe5a
arakikeita <araki1728@gmail.com>
2023-01-30 18:20:41 +0900

    ログイン状態による遷移実装

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,8 +1,16 @@
 class BuysController < ApplicationController
+  before_action :authenticate_user!, only: [:index]
+
+
+
 
   def index
     @item = Item.find(params[:item_id])
     @buy_address = BuyAddress.new
+    if @item.buy.present?
+      redirect_to root_path
+    end
+
   end
 
 
